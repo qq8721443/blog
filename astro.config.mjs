c2becec46aacb351705b115cc4d01ce697af3dfd
qq8721443 <qq8721443@naver.com>
2026-03-27 01:31:36 +0900

    feat: font 설정

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,29 @@ import mdx from "@astrojs/mdx";
 
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), mdx(), sitemap()],
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "Pretendard",
+      cssVariable: "--font-pretendard",
+      options: {
+        variants: [
+          {
+            weight: "100 900",
+            src: ["./src/assets/fonts/PretendardVariable.woff2"],
+          },
+        ],
+      },
+    },
+    {
+      provider: fontProviders.googleicons(),
+      name: "Material Icons",
+      cssVariable: "--font-material-icons",
+    },
+  ],
 });
