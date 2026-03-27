419b477440db0c782243630da3d56de636ef4369
qq8721443 <qq8721443@naver.com>
2026-03-27 17:55:35 +0900

    feat: content collection 설정

---
+++ src/content.config.ts
@@ -0,0 +1,9 @@
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
+
+const blog = defineCollection({
+  loader: glob({ pattern: "**/*.mdx", base: "./src/data/posts" }),
+});
+
+export const collections = { blog };
